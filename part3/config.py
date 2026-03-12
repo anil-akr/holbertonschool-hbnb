bae0fe892fd0ba80571c1fd7919d296bf03c0cdc
Melopheelo12 <ryan12052002@gmail.com>
2026-03-12 09:57:28 +0000

    update config

--- part3/config.py
+++ part3/config.py
@@ -9,3 +9,12 @@ config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
 }
+
+class Config:
+
+    SECRET_KEY = "secret-key"
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///hbnb.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_SECRET_KEY = "jwt-secret"
